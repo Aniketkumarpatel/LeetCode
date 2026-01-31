0d6f90d53778ff0c9dada9d0421a3639c3cc8968
Aniketkumarpatel <apatel08011@gmail.com>
2026-01-31 22:20:45 +0530

    Time: 3 ms (87.87%) | Memory: 43.2 MB (82.20%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            // opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } 
+            // closing brackets
+            else {
+                if (st.isEmpty()) return false;
+
+                char top = st.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
