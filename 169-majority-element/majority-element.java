3277b574cf0ef2473070e147112aead7ccb132da
Aniketkumarpatel <apatel08011@gmail.com>
2026-02-18 23:25:41 +0530

    Time: 1892 ms (5.01%) | Memory: 56.1 MB (5.62%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,28 @@
+ class Solution {
+
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+
+        for (int val : nums) {
+            int count = 0;
+
+            for (int el : nums) {
+                if (el == val) {
+                    count++;
+                }
+            }
+
+            if (count > n / 2) {
+                return val;
+            }
+        }
+
+        return -1; // never reached (given majority exists)
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        int[] num = {3, 2, 3};
+        System.out.println(obj.majorityElement(num));
+    }
+}
